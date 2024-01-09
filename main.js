63bfd13f28f19ae44ca02edff4071c48fa97ced1
omletccblpom <omletccblpom@yandex.ru>
2024-01-09 14:02:47 +0300

    we try

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-//fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=7b693a08-10b5-41e0-b5bc-ad8203f49f31')
-fetch('https://edu.std-900.ist.mospolytech.ru/api/routes?api_key=7b693a08-10b5-41e0-b5bc-ad8203f49f31')
-.then(function(response){
-   return response.json();
-})
+const API_KEY = "7b693a08-10b5-41e0-b5bc-ad8203f49f31"; 
+const URL = "https://edu.std-900.ist.mospolytech.ru/api/routes"; 
 
-.then(function(products){
-   let placeholder = document.querySelector("#data-output");
-   let out = "";
-   for(let route of products){
-      out += `
-         <tr>
-            <td>${route.id}</td>
-            <td>${route.name}</td>
-            <td>${route.description}</td>
-            <td>${route.mainObject}</td>
-            <td>${route.coords}</td>
-         </tr>
-      `;
-   }
- 
-   placeholder.innerHTML = out;
-});
+function displayRoutesTable(data) { 
+const tableBody = document.querySelector("#routetable"); 
+
+tableBody.innerHTML = ""; 
+
+for (let i = 0; i < data.length; i++) { 
+   let row =`
+            <tr class="table-row"> 
+               <td class="table-element">${data[i].name}</td> 
+               <td class="table-element">${data[i].description}</td> 
+               <td class="table-element">${data[i].mainObject}</td> 
+            </tr>`; 
+   tableBody.insertAdjacentHTML("beforeend", row); 
+   } 
+}
+
+//Получение данных о маршрутах 
+function getRouteData() { 
+   fetch(`${URL}?api_key=${API_KEY}`, { 
+     method: "GET", 
+}) 
+    .then((response) => { 
+       if (!response.ok) { 
+          throw new Error(`Ошибка ${response.status}`); 
+       } 
+       return response.json(); 
+     }) 
+     .then((data) => { 
+       console.log(data); 
+       displayRoutesTable(data, row); 
+     }); 
+} 
+
+
+getRouteData();
