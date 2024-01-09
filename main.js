86755da46de3cc05a568f2b5db2c8ca15e4e0ae5
omletccblpom <omletccblpom@yandex.ru>
2024-01-09 12:34:50 +0300

    I am not smart

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 //fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=7b693a08-10b5-41e0-b5bc-ad8203f49f31')
 fetch('https://edu.std-900.ist.mospolytech.ru/api/routes?api_key=7b693a08-10b5-41e0-b5bc-ad8203f49f31')
-   .then(response => response.json())
-   .then(data => console.log(data))
-
-/*.then(function(response){
+.then(function(response){
    return response.json();
 })
-.then(function(routes){
-   let placeholder = document.querySelector("#routetable");
+
+.then(function(products){
+   let placeholder = document.querySelector("#data-output");
    let out = "";
-   for(let route of routes){
+   for(let route of products){
       out += `
          <tr>
             <td>${route.id}</td>
@@ -20,5 +18,6 @@ fetch('https://edu.std-900.ist.mospolytech.ru/api/routes?api_key=7b693a08-10b5-4
          </tr>
       `;
    }
+ 
    placeholder.innerHTML = out;
-});*/
+});
